d6838650306f39fd89e77fa0125071b080caf248
Kamran Yaseen <kamranmyaseen@gmail.com>
2017-12-28 13:21:27 +0300

    Spring Eureka Server
    Spring Eureka Server

---
+++ Spring-Eureka-Server/src/main/java/com/kamranyaseen/EurekaServer/SpringEurekaServer/SpringEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.kamranyaseen.EurekaServer.SpringEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServerApplication.class, args);
+	}
+}
